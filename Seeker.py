b3f158fb11539a04862d3ef68df64ed9c4ff8657
seanjan10 <49081307+seanjan10@users.noreply.github.com>
2020-10-30 13:53:09 -0400

    Update Seeker.py
    added additional job stuff

--- Seeker.py
+++ Seeker.py
@@ -1,21 +1,21 @@
 import socket
 
+
 def Main():
-    #add creator host IP's here
-    host = ['127.0.0.1','127.0.0.1']
-    port = [5000,5001]
-     
-    
-    i=-1
-    while True: 
-        i+=1
-        if i==len(host):
-            i=0
+    # add creator host IP's here
+    host = ['127.0.0.1', '127.0.0.1']
+    port = [5000, 5001]
+
+    i = -1
+    while True:
+        i += 1
+        if i == len(host):
+            i = 0
         while True:
             try:
                 print("Asking " + host[i] + ":" + str(port[i]) + " for a job...")
                 mySocket = socket.socket()
-                mySocket.connect((host[i],port[i]))
+                mySocket.connect((host[i], port[i]))
                 break
             except:
                 break
@@ -30,7 +30,7 @@ def Main():
         else:
             data = str(data)
             print("Job found from creater : " + data)
-            status=input("Accept job? Y/N: ")
+            status = input("Accept job? Y/N: ").upper()
             if status == "Y":
                 data = data.split(",")
                 jobType = data[0]
@@ -39,11 +39,34 @@ def Main():
                 jobInput = data[3]
                 if jobType == "revStr":
                     jobInput = jobInput[::-1]
-                    mySocket.send(jobInput.encode()) 
+                    mySocket.send(jobInput.encode())
                     print("Job Complete")
+                elif jobType == "Addition":
+                    numbers = jobInput.split("+")
+                    a = int(numbers[0])
+                    b = int(numbers[1])
+                    c = a + b
+                    mySocket.send(str(c).encode())
+                    print("Job Complete")
+                elif jobType == "Subtraction":
+                    numbers = jobInput.split("-")
+                    a = int(numbers[0])
+                    b = int(numbers[1])
+                    c = a - b
+                    mySocket.send(str(c).encode())
+                    print("Job Complete")
+                elif jobType == "Multiplication":
+                    numbers = jobInput.split("x")
+                    a = int(numbers[0])
+                    b = int(numbers[1])
+                    c = a * b
+                    mySocket.send(str(c).encode())
+                    print("Job Complete")
+
             else:
-                mySocket.send("refuse".encode()) 
+                mySocket.send("refuse".encode())
         mySocket.close()
- 
+
+
 if __name__ == '__main__':
     Main()

